b059a95772c07b344568d06abfca2b5dc05d5734
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-01-14 10:00:01 +0100

    extract refactor functionalities

--- src/statement.ts
+++ src/statement.ts
@@ -13,47 +13,82 @@ type PerformanceSummary = {
   performances: Performance[];
 };
 
-export function statement(summary: PerformanceSummary, plays: Record<string, Play>) {
+export function statement(
+  summary: PerformanceSummary,
+  plays: Record<string, Play>
+) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${summary.customer}\n`;
+
+  for (let perf of summary.performances) {
+    const play = plays[perf.playID];
+    const thisAmount = getAmountPerType(play.type, perf.audience);
+    // add volume credits
+    volumeCredits += getExtraCredits(play.type, perf.audience);
+
+    // print line for this order
+    result += lineOrderDescription({
+      name: play.name,
+      amount: thisAmount,
+      audience: perf.audience,
+    });
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${formatAmount(totalAmount)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function getAmountPerType(type: string, performanceAudience: number) {
+  let amount = 0;
+
+  switch (type) {
+    case "tragedy":
+      amount = 40000;
+      if (performanceAudience > 30) {
+        amount += 1000 * (performanceAudience - 30);
+      }
+      break;
+    case "comedy":
+      amount = 30000;
+      if (performanceAudience > 20) {
+        amount += 10000 + 500 * (performanceAudience - 20);
+      }
+      amount += 300 * performanceAudience;
+      break;
+    default:
+      throw new Error(`unknown type: ${type}`);
+  }
+
+  return amount;
+}
+
+function getExtraCredits(type: string, performanceAudience: number) {
+  let extraCredits = Math.max(performanceAudience - 30, 0)
+  // add extra credit for every ten comedy attendees
+  if ("comedy" === type) extraCredits += Math.floor(performanceAudience / 5);
+
+  return extraCredits;
+}
+
+function lineOrderDescription({
+  name,
+  amount,
+  audience,
+}: {
+  name: string;
+  amount: number;
+  audience: number;
+}) {
+  return ` ${name}: ${formatAmount(amount)} (${audience} seats)\n`;
+}
+
+function formatAmount(amount: number) {
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
-
-  for (let perf of summary.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  return format(amount / 100);
 }
