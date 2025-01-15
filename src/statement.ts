61518716aa831858254560decce78661b8479576
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-01-15 17:17:45 +0100

    split into files

--- src/statement.ts
+++ src/statement.ts
@@ -8,22 +8,23 @@ export function statement(
 ) {
   let totalAmount = 0;
   let volumeCredits = 0;
+
   let statementOutput = renderStatementHeader(summary.customer);
 
   for (const performance of summary.performances) {
     const play = plays[performance.playID];
     const thisAmount = getAmountPerType(play.type, performance.audience);
-    // add volume credits
     volumeCredits += getExtraCredits(play.type, performance.audience);
 
-    // print line for this order
     statementOutput += renderStatementLineOrder({
       name: play.name,
       amount: thisAmount,
       audience: performance.audience,
     });
+
     totalAmount += thisAmount;
   }
+
   statementOutput += renderFooter(totalAmount, volumeCredits);
 
   return statementOutput;
